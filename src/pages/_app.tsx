033a77a32e43e41d48b7b3fb007d8926c8ded7a4
Claudio Ferreira <claudio.ferreira@aizoon.it>
2021-04-22 20:31:55 +0200

    Day 3 NLW5

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../styles/global.scss';
+
+import Header from '../components/Header';
+
+import styles from '../styles/app.module.scss';
+import Player from '../components/Player';
+import { PlayerContext } from '../contexts/PlayerContext';
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<PlayerContext.Provider value={'Claudio'}>
+			<div className={styles.wrapper}>
+				<main>
+					<Header />
+					<Component {...pageProps} />;
+				</main>
+				<Player />
+			</div>
+		</PlayerContext.Provider>
+	);
+}
+
+export default MyApp;
